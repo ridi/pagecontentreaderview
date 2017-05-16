140e70493e352b582ab5ca856dbd48f40e3eb909
kering <kering@ridi.com>
2017-05-16 12:47:18 +0900

    Add FitMode.WIDTH_70, WIDTH_50

--- src/main/java/com/ridi/books/viewer/reader/pagecontent/FitMode.java
+++ src/main/java/com/ridi/books/viewer/reader/pagecontent/FitMode.java
@@ -4,12 +4,16 @@ package com.ridi.books.viewer.reader.pagecontent;
  * Created by kering on 2017. 5. 12..
  */
 public enum FitMode {
-    AUTO, WIDTH, HEIGHT;
+    AUTO, WIDTH, WIDTH_70, WIDTH_50, HEIGHT;
 
     float calculateScale(SizeF contentSize, int targetWidth, int targetHeight) {
         switch (this) {
             case WIDTH:
                 return targetWidth / contentSize.width;
+            case WIDTH_70:
+                return targetWidth * 0.7f / contentSize.width;
+            case WIDTH_50:
+                return targetWidth * 0.5f / contentSize.width;
             case HEIGHT:
                 return targetHeight / contentSize.height;
             default:

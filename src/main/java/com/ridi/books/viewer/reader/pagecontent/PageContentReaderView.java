fb37e1febac66c90aef8da4a3b3275ab051a78c2
JSpiner <jspiner@naver.com>
2019-02-13 10:54:12 +0900

    childView 요소가 null인 오류 임시대응

--- src/main/java/com/ridi/books/viewer/reader/pagecontent/PageContentReaderView.java
+++ src/main/java/com/ridi/books/viewer/reader/pagecontent/PageContentReaderView.java
@@ -1364,6 +1364,13 @@ public class PageContentReaderView extends AdapterView<PageContentViewAdapter>
     }
 
     public PageContentView.Size getRenderSize() {
-        return childViews.get(currentIndex).getRenderSize();
+        PageContentView currentView = childViews.get(currentIndex);
+        if (currentView != null) {
+            return childViews.get(currentIndex).getRenderSize();
+        } else {
+            // TODO : 원인 확인 필요
+            // 알 수 없는 이유로 current index가 비어있을경우 사이즈가 같을 가능성이 높은 첫번재 뷰의 값을 리턴함.
+            return childViews.get(childViews.keyAt(0)).getRenderSize();
+        }
     }
 }

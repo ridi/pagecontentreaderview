4d207fcff992a53b7dd2d4293df33c5a1509822c
LeeSangHoon <hun6728@ridi.com>
2020-02-24 00:49:15 +0900

    Revert "PageContent.getLinkList() Nullable할 수 있도록 처리"
    This reverts commit d745a0f1b8d81462dd90af9cfb2d799bd3f56bc7.

--- src/main/java/com/ridi/books/viewer/reader/pagecontent/PageContent.java
+++ src/main/java/com/ridi/books/viewer/reader/pagecontent/PageContent.java
@@ -1,7 +1,6 @@
 package com.ridi.books.viewer.reader.pagecontent;
 
 import android.graphics.Bitmap;
-import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 
 import java.util.List;
@@ -11,6 +10,5 @@ public interface PageContent {
     @WorkerThread
     Bitmap renderToBitmap(int bitmapWidth, int bitmapHeight, int startX,
                           int startY, int pageWidth, int pageHeight, boolean forHighQuality);
-    @Nullable
     List<Link> getLinkList();
 }
